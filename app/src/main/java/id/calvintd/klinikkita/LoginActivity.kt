9bc874349836b9f1073748ec281bcf3bc68c7637
Calvin Tandra <calvin.td43@gmail.com>
2020-03-09 11:25:22 +0700

    Setting up login activity and adding test click listeners

--- app/src/main/java/id/calvintd/klinikkita/LoginActivity.kt
+++ app/src/main/java/id/calvintd/klinikkita/LoginActivity.kt
@@ -2,10 +2,26 @@ package id.calvintd.klinikkita
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var imgPasien: ImageView
+    private lateinit var imgKlinik: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        imgPasien = findViewById(R.id.imgPasien)
+        imgKlinik = findViewById(R.id.imgKlinik)
+
+        imgPasien.setOnClickListener {
+            Toast.makeText(this, R.string.test, Toast.LENGTH_SHORT).show()
+        }
+
+        imgKlinik.setOnClickListener {
+            Toast.makeText(this, R.string.test, Toast.LENGTH_SHORT).show()
+        }
     }
 }
