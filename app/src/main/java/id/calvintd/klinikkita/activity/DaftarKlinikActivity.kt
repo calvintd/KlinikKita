fedd1ec2086300cb2d38090a25ad9ae20638ee7a
Calvin Tandra <calvin.td43@gmail.com>
2020-03-11 21:58:00 +0700

    Persiapan membuat form pendaftaran klinik

---
+++ app/src/main/java/id/calvintd/klinikkita/activity/DaftarKlinikActivity.kt
@@ -0,0 +1,13 @@
+package id.calvintd.klinikkita.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.calvintd.klinikkita.R
+
+class DaftarKlinikActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_klinik)
+    }
+}
