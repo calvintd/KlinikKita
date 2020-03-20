6225bda81a56a09a5ce2ddd256d6000e7dc8bea6
Calvin Tandra <calvin.td43@gmail.com>
2020-03-20 23:45:18 +0700

    Finished designing

--- app/src/main/java/id/calvintd/klinikkita/activity/klinik/pemeriksaan/MenuPemeriksaanKlinikActivity.kt
+++ app/src/main/java/id/calvintd/klinikkita/activity/klinik/pemeriksaan/MenuPemeriksaanKlinikActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.calvintd.klinikkita.R
 
-class MenuPemeriksaanActivity : AppCompatActivity() {
+class MenuPemeriksaanKlinikActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_pemeriksaan)
+        setContentView(R.layout.activity_menu_pemeriksaan_klinik)
     }
 }
--- app/src/main/java/id/calvintd/klinikkita/activity/pasien/pemeriksaan/MenuPemeriksaanPasienActivity.kt
+++ app/src/main/java/id/calvintd/klinikkita/activity/pasien/pemeriksaan/MenuPemeriksaanPasienActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.calvintd.klinikkita.R
 
-class BerandaPemeriksaanActivity : AppCompatActivity() {
+class MenuPemeriksaanPasienActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_beranda_pemeriksaan)
+        setContentView(R.layout.activity_menu_pemeriksaan_pasien)
     }
 }
