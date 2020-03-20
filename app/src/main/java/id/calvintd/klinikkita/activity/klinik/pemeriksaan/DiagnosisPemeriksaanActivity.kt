584100c27a88467b4adf0d58918d35984ac62220
Calvin Tandra <calvin.td43@gmail.com>
2020-03-20 12:54:04 +0700

    Melanjutkan proses desain (tinggal pemeriksaan pasien)

--- app/src/main/java/id/calvintd/klinikkita/activity/klinik/pemeriksaan/DiagnosisPemeriksaanActivity.kt
+++ app/src/main/java/id/calvintd/klinikkita/activity/klinik/pemeriksaan/DiagnosisPemeriksaanActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.calvintd.klinikkita.R
 
-class CekPendaftaranKlinikActivity : AppCompatActivity() {
+class DiagnosisPemeriksaanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cek_pendaftaran_klinik)
+        setContentView(R.layout.activity_diagnosis_pemeriksaan)
     }
 }
