d964b74bead76cc16d31d8c1499a2be05485eaf7
Calvin Tandra <calvin.td43@gmail.com>
2020-03-29 20:51:37 +0700

    mengubah format waktu riwayat pemeriksaan

--- app/src/main/java/id/calvintd/klinikkita/util/DateTimeConverter.kt
+++ app/src/main/java/id/calvintd/klinikkita/util/DateTimeConverter.kt
@@ -7,7 +7,7 @@ import java.util.*
 object DateTimeConverter {
     const val utc = 7L
     @SuppressLint("SimpleDateFormat")
-    private val formatter = SimpleDateFormat("EEEE, d MMMM u, HH:mm:ss")
+    private val formatter = SimpleDateFormat("EEEE, d MMMM YYYY, HH:mm")
 
     fun konversi(timeInMillis: Long): String {
         val date = Date(timeInMillis)
